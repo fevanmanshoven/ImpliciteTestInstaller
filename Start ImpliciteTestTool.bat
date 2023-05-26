44dd1c558921e0897a5280f35b2d4ed371ae4f24
Fe Vanmanshoven <47741633+fevanmanshoven@users.noreply.github.com>
2023-05-26 10:48:35 +0200

    Add files via upload

---
+++ Start ImpliciteTestTool.bat
@@ -0,0 +1,16 @@
+@echo off
+echo "Make sure Docker is running..."
+echo "Installing application..."
+docker pull ghcr.io/fevanmanshoven/docker-implicite-test:main >nul 2>&1
+echo "Starting application..."
+docker run -it -d -p 5000:80 --name ImpliciteTest ghcr.io/fevanmanshoven/docker-implicite-test:main >nul 2>&1
+echo "Running application..."
+docker start ImpliciteTest >nul 2>&1
+echo "Loading..."
+timeout 5 >nul 2>&1
+echo "Press enter to open application. Keep this open while using the app."
+pause >nul 2>&1
+start http://localhost:5000
+echo "Press enter to exit application. Keep this open while using the app."
+pause >nul 2>&1
+docker stop ImpliciteTest
