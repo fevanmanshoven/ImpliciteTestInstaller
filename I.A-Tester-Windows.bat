4119cf1122e2599730a77a0b69eaf05708ad8e1d
Fe Vanmanshoven <47741633+fevanmanshoven@users.noreply.github.com>
2023-08-24 12:33:21 +0200

    Update and rename Start ImpliciteTestTool.bat to I.A-Tester-Windows.bat

--- I.A-Tester-Windows.bat
+++ I.A-Tester-Windows.bat
@@ -3,9 +3,9 @@ echo "Make sure Docker is running..."
 echo "Installing application..."
 docker pull ghcr.io/fevanmanshoven/docker-implicite-test:main >nul 2>&1
 echo "Starting application..."
-docker run -it -d -p 5000:80 --name ImpliciteTest ghcr.io/fevanmanshoven/docker-implicite-test:main >nul 2>&1
+docker run -it -d -p 5000:80 --name ImpliciteTestWindows ghcr.io/fevanmanshoven/docker-implicite-test:main >nul 2>&1
 echo "Running application..."
-docker start ImpliciteTest >nul 2>&1
+docker start ImpliciteTestWindows >nul 2>&1
 echo "Loading..."
 timeout 5 >nul 2>&1
 echo "Press enter to open application. Keep this open while using the app."
@@ -13,4 +13,4 @@ pause >nul 2>&1
 start http://localhost:5000
 echo "Press enter to exit application. Keep this open while using the app."
 pause >nul 2>&1
-docker stop ImpliciteTest
+docker stop ImpliciteTestWindows
